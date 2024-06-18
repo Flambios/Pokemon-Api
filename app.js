2bca207fbf99bc453de068cafaa8d7125986abc4
chris <christophe.allan.simon@gmail.com>
2024-06-18 16:40:35 +0200

    node version

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const sequelize = require('./src/db/sequelize')
 
 const app = express()
 const port = process.env.PORT || 3000
+
 console.log(process.env.PORT)
 app
     .use(favicon(__dirname + '/favicon.ico'))
